8a915a27a5d5a590ec4c1916df7e42a213ad4d19
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2023-03-22 17:11:39 +0300

    Consume upbound/provider-aws PR 616 (Move authentication down to Terraform)
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -35,14 +35,13 @@ import (
 
 func main() {
 	var (
-		app                   = kingpin.New(filepath.Base(os.Args[0]), "AWS support for Crossplane.").DefaultEnvars()
-		debug                 = app.Flag("debug", "Run with debug logging.").Short('d').Bool()
-		syncInterval          = app.Flag("sync", "Sync interval controls how often all resources will be double checked for drift.").Short('s').Default("1h").Duration()
-		pollInterval          = app.Flag("poll", "Poll interval controls how often an individual resource should be checked for drift.").Default("10m").Duration()
-		leaderElection        = app.Flag("leader-election", "Use leader election for the controller manager.").Short('l').Default("false").OverrideDefaultFromEnvar("LEADER_ELECTION").Bool()
-		maxReconcileRate      = app.Flag("max-reconcile-rate", "The global maximum rate per second at which resources may be checked for drift from the desired state.").Default("10").Int()
-		pluginProcessTTL      = app.Flag("provider-ttl", "TTL for the native plugin processes before they are replaced..").Default("100").Int()
-		useWorkspaceScheduler = app.Flag("use-ws-scheduler", "Always use the workspace provider scheduler.").Bool()
+		app              = kingpin.New(filepath.Base(os.Args[0]), "AWS support for Crossplane.").DefaultEnvars()
+		debug            = app.Flag("debug", "Run with debug logging.").Short('d').Bool()
+		syncInterval     = app.Flag("sync", "Sync interval controls how often all resources will be double checked for drift.").Short('s').Default("1h").Duration()
+		pollInterval     = app.Flag("poll", "Poll interval controls how often an individual resource should be checked for drift.").Default("10m").Duration()
+		leaderElection   = app.Flag("leader-election", "Use leader election for the controller manager.").Short('l').Default("false").OverrideDefaultFromEnvar("LEADER_ELECTION").Bool()
+		maxReconcileRate = app.Flag("max-reconcile-rate", "The global maximum rate per second at which resources may be checked for drift from the desired state.").Default("10").Int()
+		pluginProcessTTL = app.Flag("provider-ttl", "TTL for the native plugin processes before they are replaced..").Default("100").Int()
 
 		namespace                  = app.Flag("namespace", "Namespace used to set as default scope in default secret store config.").Default("crossplane-system").Envar("POD_NAMESPACE").String()
 		enableExternalSecretStores = app.Flag("enable-external-secret-stores", "Enable support for ExternalSecretStores.").Default("false").Envar("ENABLE_EXTERNAL_SECRET_STORES").Bool()
@@ -52,6 +51,7 @@ func main() {
 	setupConfig.NativeProviderSource = app.Flag("terraform-provider-source", "Terraform provider source.").Required().Envar("TERRAFORM_PROVIDER_SOURCE").String()
 	setupConfig.NativeProviderVersion = app.Flag("terraform-provider-version", "Terraform provider version.").Required().Envar("TERRAFORM_PROVIDER_VERSION").String()
 	setupConfig.NativeProviderPath = app.Flag("terraform-native-provider-path", "Terraform native provider path for shared execution.").Default("").Envar("TERRAFORM_NATIVE_PROVIDER_PATH").String()
+	setupConfig.UseWorkspaceScheduler = app.Flag("use-ws-scheduler", "Always use the workspace provider scheduler.").Bool()
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
@@ -89,12 +89,7 @@ func main() {
 	// This removes some complexity for setting up development environments.
 	setupConfig.DefaultScheduler = terraform.NewNoOpProviderScheduler()
 	if len(*setupConfig.NativeProviderPath) != 0 {
-		switch {
-		case *useWorkspaceScheduler:
-			setupConfig.DefaultScheduler = terraform.NewWorkspaceProviderScheduler(log, terraform.WithNativeProviderPath(*setupConfig.NativeProviderPath), terraform.WithNativeProviderName("registry.terraform.io/"+*setupConfig.NativeProviderSource))
-		default:
-			setupConfig.DefaultScheduler = terraform.NewSharedProviderScheduler(log, *pluginProcessTTL, terraform.WithNativeProviderPath(*setupConfig.NativeProviderPath), terraform.WithNativeProviderName("registry.terraform.io/"+*setupConfig.NativeProviderSource))
-		}
+		setupConfig.DefaultScheduler = terraform.NewSharedProviderScheduler(log, *pluginProcessTTL, terraform.WithNativeProviderPath(*setupConfig.NativeProviderPath), terraform.WithNativeProviderName("registry.terraform.io/"+*setupConfig.NativeProviderSource))
 	}
 
 	o := tjcontroller.Options{
@@ -107,7 +102,7 @@ func main() {
 		},
 		Provider:       config.GetProvider(),
 		WorkspaceStore: terraform.NewWorkspaceStore(log, terraform.WithDisableInit(len(*setupConfig.NativeProviderPath) != 0), terraform.WithProcessReportInterval(*pollInterval)),
-		SetupFn:        clients.TerraformSetupBuilder(log, setupConfig),
+		SetupFn:        clients.SelectTerraformSetup(log, setupConfig),
 	}
 
 	if *enableExternalSecretStores {
