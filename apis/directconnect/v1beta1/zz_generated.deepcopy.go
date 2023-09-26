f7b93d70b04b7b93102d149dfaa29b89ada490e2
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2023-09-26 23:21:30 +0300

    Bump underlying Terraform provider to v5.17.0
    - Looks like we had to disable the generation of a total of 19 resources (17 additional resources)
    
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- apis/directconnect/v1beta1/zz_generated.deepcopy.go
+++ apis/directconnect/v1beta1/zz_generated.deepcopy.go
@@ -703,7 +703,7 @@ func (in *ConnectionObservation) DeepCopyInto(out *ConnectionObservation) {
 	}
 	if in.VlanID != nil {
 		in, out := &in.VlanID, &out.VlanID
-		*out = new(string)
+		*out = new(int64)
 		**out = **in
 	}
 }
